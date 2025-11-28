980b52f89372d0f21bd88cdaf9410074cda90c9d
Timilehin Olusegun <timiddon97@gmail.com>
2025-11-28 19:52:59 +0100

    feat(backend-api): add Ziggy for Laravel route management in JavaScript
    - Integrate Ziggy package to enable the use of Laravel named routes in JavaScript.
    - Update composer.json and package.json to include Ziggy as a dependency.
    - Create ziggy.js to define and export Ziggy routes for global access.
    - Modify bootstrap.js to make Ziggy routes available in the global window object.

--- backend-api/resources/js/bootstrap.js
+++ backend-api/resources/js/bootstrap.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
+import { Ziggy } from './ziggy';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
+
+// Make Ziggy routes available globally
+window.Ziggy = Ziggy;
